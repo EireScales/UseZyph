ea3196c420456577c211a0ae1fc264518cd753ad
EireScales <reubvasil@gmail.com>
2026-04-03 13:50:19 +0100

    fixing the fav

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          href="/favicon-32x32.png"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </head>
       <body className="antialiased">
         {children}
       </body>
